9dd4e834934ecdce06377a51b92ae0116a9672df
Samantha Warne <samanthawarne17@gmail.com>
2020-03-16 12:39:40 -0700

    Initial commit

---
+++ canvas.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html> 
+    <body>
+        <canvas id= "myCanvas" width="900" height = "200"></canvas>
+        <button onclick="start();">Flip A Coin</button>
+        
+        <div id= "buttons"> 
+            <input type ="button" id = "clear" value= "Clear">
+        </div>
+    </body>
+    <head>
+        <script src="canvas.js"></script>
+        
+    </head>
+    
+</html>
+ 
